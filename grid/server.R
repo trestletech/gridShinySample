f8510fcf60b04c3bd46958f4f74f7d4aac1ea6ac
trestletech <jeff.allen@trestletechnology.net>
2013-05-07 17:58:57 -0500

    Cleaned up timeout function.

--- grid/server.R
+++ grid/server.R
@@ -1,27 +1,27 @@
+weather <- reactiveValues()
+weather$w <- content(GET("http://query.yahooapis.com/v1/public/yql?q=select%20item%20from%20weather.forecast%20where%20location%3D%2248907%22&format=json"))        
+
+observe({        
+  print("weather")      
+  invalidateLater(15 * 60 * 1000)  
+  weather$w <- content(GET("http://query.yahooapis.com/v1/public/yql?q=select%20item%20from%20weather.forecast%20where%20location%3D%2248907%22&format=json"))        
+})
+
 shinyServer(function(input, output) {
     
-  #update every 15 minutes
-  weatherTimer <- reactiveTimer(1 * 60 * 1000)
-  
-  weather <- reactive({        
-    print("weather")    
-    weatherTimer()
-    content(GET("http://query.yahooapis.com/v1/public/yql?q=select%20item%20from%20weather.forecast%20where%20location%3D%2248907%22&format=json"))        
-  })
-  
   output$title <- renderText({
-    "Weather"
+    "Lansing Weather"
   })
   
   output$temp <- renderText({
-    weather()$query$results$channel$item$condition$temp
+    weather$w$query$results$channel$item$condition$temp
   })
   
   output$condition <- renderText({
-    weather()$query$results$channel$item$condition$text    
+    weather$w$query$results$channel$item$condition$text    
   })
   
   output$updatedAt <- renderText({
-    paste("Updated at", gsub(".*(\\d{1,2}:\\d{2}\\s?[apAP][mM]).*", "\\1", weather()$query$results$channel$item$condition$date))
+    paste("Updated at", gsub(".*(\\d{1,2}:\\d{2}\\s?[apAP][mM]).*", "\\1", weather$w$query$results$channel$item$condition$date))
   })
 })
